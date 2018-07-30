05e9de0e8ca67568c6f6941bd9f4952ca9663d3b
Chan Park <cwal1220@naver.com>
2018-07-30 09:14:01 +0900

    no message

--- application/app/src/main/java/com/example/cwal1/mqtt/MainActivity.java
+++ application/app/src/main/java/com/example/cwal1/mqtt/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.cwal1.mqtt;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
@@ -19,20 +22,32 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
     TextView textView;
+    EditText editText_topic;
+    Button button_subscribe, button_clear;
     private MqttClient client;
-    String topic = "hello/world";
+    String topic = "hello/+";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.textView);
+        // Widgets
+        textView            = findViewById(R.id.textView);
+        editText_topic      = findViewById(R.id.editText_topic);
+        button_subscribe    = findViewById(R.id.button_subscribe);
+        button_clear        = findViewById(R.id.button_clear);
+        editText_topic.setText(topic);
+
         String clientId = MqttClient.generateClientId();
         try {
             client = new MqttClient("tcp://49.236.136.179:1883", clientId, new MemoryPersistence());
             client.connect();
-            client.publish(topic, new MqttMessage(new String("Hello MQTT !").getBytes()));
+            // hello message
+            client.publish("hello/world", new MqttMessage(new String("Hello MQTT !").getBytes()));
             client.subscribe(topic, new IMqttMessageListener() {
                 @Override
                 public void messageArrived(final String topic, final MqttMessage message) throws Exception {
@@ -41,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
                     {
                         public void run()
                         {
-                            textView.append("\nTopic : " + topic + ", Message : " + message.toString());
+                            textView.append((new SimpleDateFormat("YYYY-MM-dd hh:mm:ss")).format(new Date())+"\n          Topic : " + topic + "\tMessage : " + message.toString() + "\n");
                         }
                     });
 
@@ -52,6 +67,40 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "error!", Toast.LENGTH_LONG).show();
         }
 
+        // 구독 버튼 클릭시 기존 토픽 구독 해제 후 입력받은 토픽 구독
+        button_subscribe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    client.unsubscribe(topic);
+                    topic = editText_topic.getText().toString();
+                    client.subscribe(topic, new IMqttMessageListener() {
+                        @Override
+                        public void messageArrived(final String topic, final MqttMessage message) throws Exception {
+                            Log.d(topic, message.toString());
+                            runOnUiThread(new Runnable()
+                            {
+                                public void run()
+                                {
+                                    textView.append((new SimpleDateFormat("YYYY-MM-dd hh:mm:ss")).format(new Date())+"\n          Topic : " + topic + "\tMessage : " + message.toString() + "\n");
+                                }
+                            });
+
+                        }
+                    });
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        // 삭제 버튼 클릭시 내용 제거
+        button_clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("");
+            }
+        });
 
     }
 
